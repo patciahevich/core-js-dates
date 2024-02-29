2e3373f92e7c843db46dbcbe7625abb26d4714ab
Volha Patciahevich <zaprudskaya@gmail.com>
2024-02-29 15:46:09 +0300

    feat: solve getNextFriday task

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -79,8 +79,17 @@ function getDayName(date) {
  * Date('2024-02-13T00:00:00Z') => Date('2024-02-16T00:00:00Z')
  * Date('2024-02-16T00:00:00Z') => Date('2024-02-23T00:00:00Z')
  */
-function getNextFriday(/* date */) {
-  throw new Error('Not implemented');
+function getNextFriday(date) {
+  const day = new Date(date).getDay();
+  let nextDay = 5 - day;
+
+  if (nextDay <= 0) {
+    nextDay = 7 + nextDay;
+  }
+
+  const ms = nextDay * 24 * 3600 * 1000;
+
+  return new Date(Date.parse(date) + ms);
 }
 
 /**
