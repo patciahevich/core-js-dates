3b3ed7e746ec68b8023ebfa9c8adc129302883b7
Volha Patciahevich <zaprudskaya@gmail.com>
2024-03-03 20:12:55 +0300

    feat: solve getNextFridayThe13th task

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -245,8 +245,17 @@ function getWeekNumberByDate(date) {
  * Date(2024, 0, 13) => Date(2024, 8, 13)
  * Date(2023, 1, 1) => Date(2023, 9, 13)
  */
-function getNextFridayThe13th(/* date */) {
-  throw new Error('Not implemented');
+function getNextFridayThe13th(date) {
+  const month = date.getMonth();
+  const year = date.getFullYear();
+
+  for (let i = month; i < 12; i += 1) {
+    const day = new Date(year, i, 13);
+    if (day.getDay() === 5) {
+      return day;
+    }
+  }
+  return -1;
 }
 
 /**
